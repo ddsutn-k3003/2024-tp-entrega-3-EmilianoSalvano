6502abdd6f02ee9c46b0c6fa8708bfbce5822da0
Emiliano Martin Salvano <82066523+EmilianoSalvano@users.noreply.github.com>
2024-06-13 16:50:50 -0300

    Testeando la config de la db

--- src/main/java/ar/edu/utn/dds/k3003/model/Colaborador.java
+++ src/main/java/ar/edu/utn/dds/k3003/model/Colaborador.java
@@ -2,12 +2,29 @@ package ar.edu.utn.dds.k3003.model;
 
 import ar.edu.utn.dds.k3003.facades.dtos.FormaDeColaborarEnum;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "colaboradores")
 public class Colaborador {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long ID;
+
+  //@Column(name = "formas_de_colaborar")
+  @Transient
   private List<FormaDeColaborarEnum> formasDeColaborar;
+
+  @Column(name = "nombre")
   private String nombre;
   //private List<Contribucion> contribuciones;
 
